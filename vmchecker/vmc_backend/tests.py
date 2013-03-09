cda384bd203ee6b7c64cf868c243d64a0a9780d6
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-09 12:54:55 +0200

    Changed tests.py

--- vmchecker/vmc_backend/tests.py
+++ vmchecker/vmc_backend/tests.py
@@ -4,9 +4,12 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
-
+from django.conf import settings
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+import logging
+from models import Subject, Assignment, UsersToSubjects, Submission,\
+    SubmissionComment
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
