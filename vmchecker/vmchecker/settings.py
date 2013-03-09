bdeb4ab97eb281e19524e13622290bbb8e2a55bd
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-03-09 12:46:50 +0200

    Renamed stuff, made admin to work, added database

--- vmchecker/vmchecker/settings.py
+++ vmchecker/vmchecker/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'vmc_backend/vmc_db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -123,7 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'vmc-backend',
+    'vmc_backend',
 )
 
 # A sample logging configuration. The only tangible logging
