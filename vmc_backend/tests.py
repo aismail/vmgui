fb0dbcadb1399e815782b16e3b79058f05aef9e5
Bogdan Bresug <bogdan.bresug@gmail.com>
2013-03-09 16:46:36 +0200

    Added test to see if Assignment's attachment field is optional

--- vmc_backend/tests.py
+++ vmc_backend/tests.py
@@ -4,6 +4,8 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import datetime
+from django.utils.timezone import utc
 from django.conf import settings
 from django.test import TestCase
 from django.contrib.auth.models import User
@@ -17,3 +19,16 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class AssignmentModelTest(TestCase):
+
+    #checks if the attachments field is set as optional
+    def test_optional_field(self):
+        testsubject = Subject(name = "testname", description = "testdescription", link = "http://www.google.ro/")
+        testsubject.save()
+        now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        testassignment = Assignment(subject_id = testsubject, name="test", text = "asda", deadline = now)
+        testassignment.save()
+        testsubject.delete()
+        testassignment.delete()
+
