cd4d7b8a945a6d9fe9288cbd8a8158e6951527dd
Andrei Duma <andrei.duma.dorian@gmail.com>
2013-03-09 13:46:24 +0200

    Changed required/optional fields in Subject and SubmissionComment Models.

--- vmc_backend/models.py
+++ vmc_backend/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 
 class Subject(models.Model):
     name = models.CharField(max_length=30)
-    description = models.TextField()
-    link = models.URLField(max_length=200)
+    description = models.TextField(blank=True)
+    link = models.URLField(max_length=200, blank=True)
+    contact_person_email = models.EmailField(blank=True)
 
     def __str__(self):
         return self.name
@@ -42,15 +43,20 @@ class Submission(models.Model):
     uploaded_at = models.DateTimeField()
     graded = models.BooleanField(default=False)
     comment_count = models.IntegerField()
+<<<<<<< HEAD
     content = models.FileField(upload_to='files')
 
+=======
+    content = models.FileField(upload_to='vmc_backend/files')
+    
+>>>>>>> Changed required/optional fields in Subject and SubmissionComment Models.
     def __str__(self):
         return str(self.student_id) + "-" + str(self.assignment_id) + "-" + str(self.uploaded_at)
 
 class SubmissionComment(models.Model):
     submission_id = models.ForeignKey(Submission)
-    filename = models.CharField(max_length=256)
-    line_no = models.IntegerField()
+    filename = models.CharField(max_length=256, blank=True)
+    line_no = models.IntegerField(blank=True)
     comment_no = models.IntegerField()
     comment = models.TextField()
 
