79b9c8cc6ebf52e986916f32dc5851f025030ccc
Bogdan Bresug <bogdan.bresug@gmail.com>
2013-03-09 11:32:31 +0200

    Added assignment model

--- vmchecker/backend/models.py
+++ vmchecker/backend/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Subject(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     link = models.URLField(max_length=200)
-    
+    contact_person_id = models.ForeignKey(User)
+
+class Assignment(models.Model):
+    subject_id = models.ForeignKey(Subject)
+    text = models.CharField(max_length=2000)
+    deadline = models.DateField()
+    attachments = models.URLField()
