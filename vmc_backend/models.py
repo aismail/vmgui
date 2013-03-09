e369d670dbaca93f88ce4a163899c328e268ac7e
Valentin Gosu <valentin.gosu@gmail.com>
2013-03-09 13:20:33 +0200

    Added str() methods to models

--- vmc_backend/models.py
+++ vmc_backend/models.py
@@ -5,13 +5,20 @@ class Subject(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     link = models.URLField(max_length=200)
+    
+    def __str__(self):
+        return self.name
 
 
 class Assignment(models.Model):
     subject_id = models.ForeignKey(Subject)
+    name = models.CharField(max_length=30)
     text = models.TextField()
     deadline = models.DateTimeField()
     attachments = models.URLField()
+    
+    def __str__(self):
+        return self.name
 
 class UsersToSubjects(models.Model):
     unique_together = ("subject_id", "user_id")
@@ -25,6 +32,9 @@ class UsersToSubjects(models.Model):
     role = models.CharField(max_length = 15,
                             choices = role_choices,
                             default = 'student')
+    
+    def __str__(self):
+        return str(self.subject_id) + "-" + str(self.user_id)
 
 class Submission(models.Model):
     student_id = models.ForeignKey(User, related_name='submissions')
@@ -33,6 +43,9 @@ class Submission(models.Model):
     graded = models.BooleanField()
     comment_count = models.IntegerField()
     content = models.FileField(upload_to='files')
+    
+    def __str__(self):
+        return str(self.student_id) + "-" + str(self.assignment_id) + "-" + str(self.uploaded_at)
 
 class SubmissionComment(models.Model):
     submission_id = models.ForeignKey(Submission)
@@ -40,3 +53,6 @@ class SubmissionComment(models.Model):
     line_no = models.IntegerField()
     comment_no = models.IntegerField()
     comment = models.TextField()
+
+    def __str__(self):
+        return str(self.submission_id) + "-" + str(self.comment_no)
