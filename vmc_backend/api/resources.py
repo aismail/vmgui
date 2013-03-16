6580fd6ed9a5362e163d82d782797207f25cdda1
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-16 11:58:11 +0200

    #36 Added submission resource

--- vmc_backend/api/resources.py
+++ vmc_backend/api/resources.py
@@ -1,1 +1,8 @@
 """ This file contains tastypie resources for all models in vmc_backend """
+from tastypie.resources import ModelResource
+from vmc_backend.models import Submission
+
+class SubmissionResource(ModelResource):
+    class Meta:
+        queryset = Submission.objects.all()
+        allowed_methods = ['get']
--- vmc_backend/api/urls.py
+++ vmc_backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 from tastypie.api import Api
-
+from vmc_backend.api.resources import SubmissionResource
 v1_api = Api(api_name='v1')
+v1_api.register(SubmissionResource())
 
 urlpatterns = patterns('',
     (r'^', include(v1_api.urls))
