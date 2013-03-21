a722fd95030081b0f4dcc3f73667030b74386e60
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-21 17:53:50 +0200

    POST test is failing

--- vmc_backend/api/resources.py
+++ vmc_backend/api/resources.py
@@ -1,13 +1,27 @@
 """ This file contains tastypie resources for all models in vmc_backend """
+from tastypie import fields
 from tastypie.resources import ModelResource
 from vmc_backend.models import Assignment, Subject, Submission
-from tastypie.authentication import BasicAuthentication
+from tastypie.authentication import Authentication
+from tastypie.authorization import Authorization
 
 class SubmissionResource(ModelResource):
+    student = fields.IntegerField(attribute='sudent_id')
+    #assignment = fields.IntegerField(attribute= 'assignment_id')
     class Meta:
         queryset = Submission.objects.all()
         allowed_methods = ['get', 'post']
-        authentication = BasicAuthentication()
+        authentication = Authentication()
+        authorization = Authorization()
+    def hydrate(self, bundle):
+        #bundle.data['student_id']=self.request.POST['student']
+        if not bundle.obj.pk:
+            bundle.obj.student_id=self.student
+        return bundle
+    def dehydrate(self, bundle):
+        from nose.tools import set_trace; set_trace()
+        bundle.data['student_id']=self.student
+        return bundle
 
 
 class AssignmentResource(ModelResource):
--- vmc_backend/api/tests/submission_resource_tests.py
+++ vmc_backend/api/tests/submission_resource_tests.py
@@ -2,31 +2,33 @@ import datetime
 from tastypie.test import ResourceTestCase
 from vmc_backend.api.resources import SubmissionResource
 from vmc_backend.factories import SubmissionFactory
+from vmc_backend.factories import AssignmentFactory
+from vmc_backend.factories import UserFactory
 from vmc_backend.models import Submission
 from django.contrib.auth.models import User
 
 
-class SubjectResourceTest(ResourceTestCase):
+class SubmissionResourceTest(ResourceTestCase):
     def setUp(self):
-        super(SubjectResourceTest, self).setUp()
+        super(SubmissionResourceTest, self).setUp()
         
         self.subm1 = SubmissionFactory()
         self.subm2 = SubmissionFactory()
         # Create a user.
-        self.username = 'user'
+        self.username = 'user111'
         self.password = 'pass'
         self.user = User.objects.create_user(self.username,
-                        'daniel@example.com', self.password)
+                                       'daniel@example.com', self.password)
 
         self.detail_url = '/api/v1/submission/{0}/'.format(self.subm1.pk)
         self.post_data = {
-            'user': '/api/v1/user/{0}/'.format(self.user.pk),  
-            'assignment_id': '<Assignment: fd27e50eb88455704dc0aa1eb4d9b5>',
-            'student_id': '<User: 4bc17e76edea0760902dd155fefae3>',
+            #'student_id': '/api/v1/user/{0}'.format(self.user.pk),
+            'student_id': self.subm1.student_id,
+            'assignment_id': '/api/v1/assignment/{0}'.format(self.subm1.assignment_id),
             'uploaded_at': datetime.datetime(2013, 3, 18, 20, 1, 35, 399386),
-            'graded': False,
+            'graded': False
         }
-         
+        #from nose.tools import set_trace; set_trace() 
     def get_credentials(self):
         return self.create_basic(username=self.username, password=self.password)
 
@@ -36,6 +38,7 @@ class SubjectResourceTest(ResourceTestCase):
     def test_get_detail_json(self):
         resp = self.api_client.get(self.detail_url, format='json', 
                                    authentication=self.get_credentials())
+        #from nose.tools import set_trace; set_trace()
         self.assertValidJSONResponse(resp)
 
     def test_get_list_json(self):
@@ -51,10 +54,8 @@ class SubjectResourceTest(ResourceTestCase):
     def test_post_list(self):
         # Check how many are there first.
         self.assertEqual(Submission.objects.count(), 2)
-        # from nose.tools import set_trace; set_trace()
+        #from nose.tools import set_trace; set_trace()
         self.assertHttpCreated(self.api_client.post('/api/v1/submission/',
-                             format='json', data=self.post_data,
-                             authentication=self.get_credentials()))
+                             ormat='json', data=self.post_data))
         # Verify a new one has been added.
         self.assertEqual(Submission.objects.count(), 3)
-
