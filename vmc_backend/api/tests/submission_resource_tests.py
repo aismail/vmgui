f8faf8792a5245674ecf74f29e6b68b0a2f160e9
Andrei Ismail <iandrei@gmail.com>
2013-03-22 09:29:48 +0200

    Fixing format + removed unnecessary unauthorized tests. (#36)

--- vmc_backend/api/tests/submission_resource_tests.py
+++ vmc_backend/api/tests/submission_resource_tests.py
@@ -20,9 +20,8 @@ class SubmissionResourceTest(ResourceTestCase):
         self.detail_url = '/api/v1/submission/%d/' % self.subm1.pk
 
         self.post_data = {
-            # 'student_id': '/api/v1/user/{0}'.format(self.user.pk),
             'student_id': self.subm1.student_id,
-            'assignment_id': '/api/v1/assignment/{0}'.format(self.subm1.assignment_id),
+            'assignment_id': self.subm1.assignment_id,
             'uploaded_at': datetime.datetime(2013, 3, 18, 20, 1, 35, 399386),
             'graded': False
         }
@@ -30,9 +29,6 @@ class SubmissionResourceTest(ResourceTestCase):
     def get_credentials(self):
         return self.create_basic(username=self.username, password=self.password)
 
-    def test_get_detail_unauthenticated(self):
-        self.assertHttpUnauthorized(self.api_client.get(self.detail_url,
-                                    format='json'))
     def test_get_detail_json(self):
         resp = self.api_client.get(self.detail_url, format='json',
                                    authentication=self.get_credentials())
@@ -45,15 +41,11 @@ class SubmissionResourceTest(ResourceTestCase):
         self.assertValidJSONResponse(resp)
         self.assertEqual(len(self.deserialize(resp)['objects']), 2)
 
-    def test_post_list_unauthenticated(self):
-        self.assertHttpUnauthorized(self.api_client.post('/api/v1/submission/',
-                                    format='json', data=self.post_data))
-
     def test_post_list(self):
         # Check how many are there first.
         self.assertEqual(Submission.objects.count(), 2)
         # from nose.tools import set_trace; set_trace()
         self.assertHttpCreated(self.api_client.post('/api/v1/submission/',
-                             ormat='json', data=self.post_data))
+                             format='json', data=self.post_data))
         # Verify a new one has been added.
         self.assertEqual(Submission.objects.count(), 3)
