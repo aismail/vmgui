96ba929e26f49c419621dd6b99ffb8c407a99a57
Andrei Ismail <iandrei@gmail.com>
2013-03-22 09:19:41 +0200

    Added detail_url + clean-up. (#36)

--- vmc_backend/api/tests/submission_resource_tests.py
+++ vmc_backend/api/tests/submission_resource_tests.py
@@ -1,9 +1,6 @@
 import datetime
 from tastypie.test import ResourceTestCase
-from vmc_backend.api.resources import SubmissionResource
 from vmc_backend.factories import SubmissionFactory
-from vmc_backend.factories import AssignmentFactory
-from vmc_backend.factories import UserFactory
 from vmc_backend.models import Submission
 from django.contrib.auth.models import User
 
@@ -11,7 +8,7 @@ from django.contrib.auth.models import User
 class SubmissionResourceTest(ResourceTestCase):
     def setUp(self):
         super(SubmissionResourceTest, self).setUp()
-        
+
         self.subm1 = SubmissionFactory()
         self.subm2 = SubmissionFactory()
         # Create a user.
@@ -19,26 +16,27 @@ class SubmissionResourceTest(ResourceTestCase):
         self.password = 'pass'
         self.user = User.objects.create_user(self.username,
                         'daniel@example.com', self.password)
-        
+
+        self.detail_url = '/api/v1/submission/%d/' % self.subm1.pk
 
         self.post_data = {
-            #'student_id': '/api/v1/user/{0}'.format(self.user.pk),
+            # 'student_id': '/api/v1/user/{0}'.format(self.user.pk),
             'student_id': self.subm1.student_id,
             'assignment_id': '/api/v1/assignment/{0}'.format(self.subm1.assignment_id),
             'uploaded_at': datetime.datetime(2013, 3, 18, 20, 1, 35, 399386),
             'graded': False
         }
-        #from nose.tools import set_trace; set_trace() 
+
     def get_credentials(self):
         return self.create_basic(username=self.username, password=self.password)
 
     def test_get_detail_unauthenticated(self):
-        self.assertHttpUnauthorized(self.api_client.get(self.detail_url, 
+        self.assertHttpUnauthorized(self.api_client.get(self.detail_url,
                                     format='json'))
     def test_get_detail_json(self):
-        resp = self.api_client.get(self.detail_url, format='json', 
+        resp = self.api_client.get(self.detail_url, format='json',
                                    authentication=self.get_credentials())
-        #from nose.tools import set_trace; set_trace()
+        # from nose.tools import set_trace; set_trace()
         self.assertValidJSONResponse(resp)
 
     def test_get_list_json(self):
@@ -49,12 +47,12 @@ class SubmissionResourceTest(ResourceTestCase):
 
     def test_post_list_unauthenticated(self):
         self.assertHttpUnauthorized(self.api_client.post('/api/v1/submission/',
-                                    format='json', data=self.post_data)) 
+                                    format='json', data=self.post_data))
 
     def test_post_list(self):
         # Check how many are there first.
         self.assertEqual(Submission.objects.count(), 2)
-        #from nose.tools import set_trace; set_trace()
+        # from nose.tools import set_trace; set_trace()
         self.assertHttpCreated(self.api_client.post('/api/v1/submission/',
                              ormat='json', data=self.post_data))
         # Verify a new one has been added.
