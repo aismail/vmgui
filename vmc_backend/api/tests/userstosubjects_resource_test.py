b96a3832c347ff8105bdc5a6d16009ccb6601794
Bogdan Bresug <bogdan.bresug@gmail.com>
2013-03-23 15:16:58 +0200

    Repaired userstosubjects tests

--- vmc_backend/api/tests/userstosubjects_resource_test.py
+++ vmc_backend/api/tests/userstosubjects_resource_test.py
@@ -8,23 +8,22 @@ class UsersToSubjectsResourceTest(ResourceTestCase):
         super(UsersToSubjectsResourceTest, self).setUp()
         self.u2s1 = UsersToSubjectsFactory()
         self.u2s2 = UsersToSubjectsFactory()
-        self.subjectwithnousers = SubjectFactory(pk=123)
+        self.base_url = '/api/v1/userstosubjects/'
 
-    def test_get_valid_json_response(self):
-        resp = self.api_client.get('/api/v1/userstosubjects/', format='json')
+    def test_get_valid_json_response_and_objects(self):
+        resp = self.api_client.get(self.base_url, format='json')
         self.assertValidJSONResponse(resp)
-
-    def test_get_all_objects_and_in_order(self):
-        resp = self.api_client.get('/api/v1/userstosubjects/', format='json')
-        self.assertEqual(len(self.deserialize(resp)['objects']), 2)
-        self.assertEqual(self.deserialize(resp)['objects'][1]['id'], self.u2s2.id)
+        deserialized_resp = self.deserialize(resp)
+        self.assertEqual(len(deserialized_resp['objects']), 2)
+        self.assertEqual(deserialized_resp['objects'][1]['id'], self.u2s2.pk)
 
     def test_filter_by_user(self):
-        resp = self.api_client.get('/api/v1/userstosubjects/?user_id=%s' %self.u2s1.user_id, format = 'json')
-        self.assertEqual(len(self.deserialize(resp)['objects']), 1)
-        self.assertEqual(self.deserialize(resp)['objects'][0]['id'], self.u2s1.id)
+        resp = self.api_client.get(self.base_url + '?user_id=%s' %self.u2s1.user_id, format = 'json')
+        deserialized_resp = self.deserialize(resp)
+        self.assertEqual(len(deserialized_resp['objects']), 1)
+        self.assertEqual(deserialized_resp['objects'][0]['id'], self.u2s1.pk)
 
-        """Tests if a subject with no users returns an empty list.
-        """
-        resp = self.api_client.get('/api/v1/userstosubjects/?user_id=%s' %self.subjectwithnousers.pk, format = 'json')
+        #Tests if a user with no userstosubjects returns an empty list.
+        user_with_no_users_to_subjects_mapping = UserFactory()
+        resp = self.api_client.get(self.base_url + '?user_id=%s' %user_with_no_users_to_subjects_mapping.pk, format = 'json')
         self.assertEqual(len(self.deserialize(resp)['objects']), 0)
