0bc71557ac354164e35fa00d23e532a1182fc85d
Didii Cosmin <cosmin.didii@gmail.com>
2013-03-23 14:32:59 +0200

    added script to autopopulate db

---
+++ vmc_backend/management/commands/populatedb.py
@@ -0,0 +1,46 @@
+import os
+import factory
+
+from django.core.management.base import BaseCommand
+from django.core import management
+from django.contrib.auth.management.commands import changepassword 
+from vmc_backend.factories import *
+
+from vmc_backend.models import Subject, Assignment, UsersToSubjects, Submission,\
+    SubmissionComment
+
+class Command(BaseCommand):
+
+  def handle(self, *args, **options):
+    #
+    def deleteDB():
+      path = os.getcwd()
+      pathToFile = path + "/vmc_backend/vmc_db"
+      try:
+        os.remove(pathToFile)
+      except OSError:
+        print 'Creating database'
+    #run the syncdb
+    def recreateDB():
+      management.call_command('syncdb', interactive=False)
+
+      # Creates the super user and sets his password.
+      management.call_command('createsuperuser', interactive=False,
+      username="cdl", email="cdl@gmail.com")
+      command = changepassword.Command()
+      command._get_pass = lambda *args: 'cdl'
+
+    
+    def generateData(): 
+      for x in range(0, 20):
+        SubjectFactory.create()
+        AssignmentFactory.create()
+        UsersToSubjectsFactory.create()
+        SubmissionFactory.create()
+        SubmissionCommentFactory.create()
+        UserFactory.create()
+    
+    deleteDB()
+    recreateDB()
+    generateData()
+  
