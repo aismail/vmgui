52480d7742c26ee730ebef56a8aca41bfa4d9ef9
Valentin Gosu <valentin.gosu@gmail.com>
2013-03-23 14:53:34 +0200

    Added file downloading view with authentication

--- vmc_backend/views.py
+++ vmc_backend/views.py
@@ -1,1 +1,22 @@
 # Create your views here.
+
+import django.contrib.auth.decorators as auth_decorators
+from django.conf import settings
+import os, django
+
+UPLOADED = settings.PROJECT_PATH+'/uploads'
+
+def get_absolute_filename(filename='', safe=True):
+    if not filename:
+        return os.path.join(UPLOADED, 'index')
+    if safe and '..' in filename.split(os.path.sep):
+        return get_absolute_filename(filename='')
+    return os.path.join(UPLOADED, filename)
+
+@auth_decorators.login_required
+def retrieve_file(request, filename=''):
+    abs_filename = get_absolute_filename(filename)
+    response = django.http.HttpResponse() # 200 OK
+    del response['content-type'] # We'll let the web server guess this.
+    response['X-Sendfile'] = abs_filename
+    return response
