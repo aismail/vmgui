281016d9b7736bd5bf727651fc74791d1375451c
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-26 09:48:10 +0200

    Added basic test for submissionform #72

---
+++ vmc_backend/tests/test_submission_form.py
@@ -0,0 +1,12 @@
+from nose.tools import eq_, ok_
+
+from vmc_backend.forms import SubmissionForm
+from vmc_backend.tests.base_model_form_test_case import BaseModelFormTestCase
+
+
+class TestSubmissionForm(BaseModelFormTestCase):
+
+    def test_form_can_be_instantiated(self):
+
+        form = SubmissionForm()
+        eq_(form.data,{})
