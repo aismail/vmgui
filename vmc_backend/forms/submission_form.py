71f35f4c84d3c51d203421807805b73222d291de
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-26 20:21:51 +0200

    Added clean method #72

--- vmc_backend/forms/submission_form.py
+++ vmc_backend/forms/submission_form.py
@@ -1,7 +1,16 @@
 from vmc_backend.core.base_model_form import BaseModelForm
-from vmc_backend.models import Submission
+from vmc_backend.models import Submission, Assignment
 
 
 class SubmissionForm(BaseModelForm):
     class Meta:
         model = Submission
+    def clean(self):
+        cleaned_data = super(SubmissionForm, self).clean()
+
+        from nose.tools import set_trace; set_trace()
+        ass = cleaned_data.get('assignment')
+
+        Assignment.objects.get(pk=ass.pk)
+
+        return cleaned_data
