0c26d766c082aa27f9a64bf055701bd8326364ff
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-26 21:56:10 +0200

    Changed clean method to properly catch the case when the assignment does not exist #72

--- vmc_backend/forms/submission_form.py
+++ vmc_backend/forms/submission_form.py
@@ -1,3 +1,5 @@
+from django.forms import ValidationError
+
 from vmc_backend.core.base_model_form import BaseModelForm
 from vmc_backend.models import Submission, Assignment
 
@@ -5,12 +7,13 @@ from vmc_backend.models import Submission, Assignment
 class SubmissionForm(BaseModelForm):
     class Meta:
         model = Submission
+
     def clean(self):
         cleaned_data = super(SubmissionForm, self).clean()
-
-        from nose.tools import set_trace; set_trace()
         ass = cleaned_data.get('assignment')
-
-        Assignment.objects.get(pk=ass.pk)
+        try:
+            Assignment.objects.get(pk=ass.pk)
+        except(AttributeError):
+            raise ValidationError('Select a valid choice.')
 
         return cleaned_data
