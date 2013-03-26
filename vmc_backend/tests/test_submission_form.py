264874fc0893b41fc322bae0f7d30211a403f1b2
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-26 10:31:57 +0200

    Added test to check if assignment exist #72

--- vmc_backend/tests/test_submission_form.py
+++ vmc_backend/tests/test_submission_form.py
@@ -1,12 +1,24 @@
-from nose.tools import eq_, ok_
+from nose.tools import eq_, ok_, raises
 
 from vmc_backend.forms.submission_form import SubmissionForm
 from vmc_backend.tests.base_model_form_test_case import BaseModelFormTestCase
+from vmc_backend.factories import SubmissionFactory, AssignmentFactory
+from vmc_backend.models import Assignment
 
 
 class TestSubmissionForm(BaseModelFormTestCase):
 
     def test_form_can_be_instantiated(self):
 
-        form = SubmissionForm()
+        subm = SubmissionFactory()
+        form = SubmissionForm(subm)
         eq_(form.data, {})
+        from nose.tools import set_trace; set_trace()
+
+    def test_assignment_exists(self):
+
+        ass = AssignmentFactory()
+        subm = SubmissionFactory(assignment=ass)
+        ass.delete()
+        self.assertRaises(Assignment.DoesNotExist,form = SubmissionForm(subm))
+
