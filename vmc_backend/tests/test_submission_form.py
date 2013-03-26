ce912ebc0f9145f68847e8d2314c587b7977cec8
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-26 11:06:47 +0200

    Added test to check if the student is enrolled at the course of the assignment #72

--- vmc_backend/tests/test_submission_form.py
+++ vmc_backend/tests/test_submission_form.py
@@ -1,8 +1,10 @@
 from nose.tools import eq_, ok_, raises
+from django.forms import ValidationError
 
 from vmc_backend.forms.submission_form import SubmissionForm
 from vmc_backend.tests.base_model_form_test_case import BaseModelFormTestCase
-from vmc_backend.factories import SubmissionFactory, AssignmentFactory
+from vmc_backend.factories import (SubmissionFactory, AssignmentFactory,
+                        UserFactory, SubjectFactory)
 from vmc_backend.models import Assignment
 
 
@@ -14,3 +16,9 @@ class TestSubmissionForm(BaseModelFormTestCase):
         ass.delete()
         self.assertRaises(Assignment.DoesNotExist,form = SubmissionForm(subm))
 
+    def test_student_is_enrolled_at_the_subject(self):
+        stud1 = UserFactory()
+        subj1 = SubjectFactory()
+        ass = AssignmentFactory(subject=subj1)
+        subm = SubmissionFactory(student=stud1, assignment=ass)
+        self.assertRaises(ValidationError, form = SubmissionForm(subm))
