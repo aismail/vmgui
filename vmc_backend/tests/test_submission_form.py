2ec88b0fb6869456233a745566899ec829da2049
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-26 11:39:12 +0200

    Added test to check if student exists #72

--- vmc_backend/tests/test_submission_form.py
+++ vmc_backend/tests/test_submission_form.py
@@ -1,5 +1,6 @@
 from nose.tools import eq_, ok_, raises
 from django.forms import ValidationError
+from django.contrib.auth.models import User
 
 from vmc_backend.forms.submission_form import SubmissionForm
 from vmc_backend.tests.base_model_form_test_case import BaseModelFormTestCase
@@ -10,11 +11,16 @@ from vmc_backend.models import Assignment
 
 class TestSubmissionForm(BaseModelFormTestCase):
     def test_assignment_exists(self):
-
         ass = AssignmentFactory()
         subm = SubmissionFactory(assignment=ass)
         ass.delete()
         self.assertRaises(Assignment.DoesNotExist, SubmissionForm, subm)
+
+    def test_student_exists(self):
+        stud = UserFactory()
+        subm = SubmissionFactory(student=stud)
+        stud.delete()
+        self.assertRaises(User.DoesNotExist, SubmissionForm, subm)
 
     def test_student_is_enrolled_at_the_subject(self):
         stud1 = UserFactory()
