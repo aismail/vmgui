31d373bd3e9f635815e8d5cd1539ea8139d89632
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-03-27 09:08:30 +0200

    Added clean method to catch cases when the student isn't enrolled at the subject of the assignment. Deleted useless validation for assignment #72

--- vmc_backend/forms/submission_form.py
+++ vmc_backend/forms/submission_form.py
@@ -1,7 +1,7 @@
 from django.forms import ValidationError
 
 from vmc_backend.core.base_model_form import BaseModelForm
-from vmc_backend.models import Submission, Assignment
+from vmc_backend.models import Submission, Assignment, UsersToSubjects
 
 
 class SubmissionForm(BaseModelForm):
@@ -9,12 +9,20 @@ class SubmissionForm(BaseModelForm):
         model = Submission
         exclude = ('uploaded_at', )
 
+    def clean_subject_and_assignment(self):
+        if ('student' in self.cleaned_data.keys() and \
+            'assignment' in self.cleaned_data.keys()): 
+            #from nose.tools import set_trace; set_trace()
+            stud = self.cleaned_data['student']
+            assignment = self.cleaned_data['assignment']
+            subj = assignment.subject
+            u2s = UsersToSubjects.objects.all().filter(user=stud, subject=subj)
+            if not u2s:
+                raise ValidationError('Student not enrolled at this subject')
+        return self.cleaned_data
+
     def clean(self):
         cleaned_data = super(SubmissionForm, self).clean()
-        assignment = cleaned_data.get('assignment')
-        try:
-            Assignment.objects.get(pk=assignment.pk)
-        except(AttributeError):
-            raise ValidationError('Select a valid choice.')
+        cleaned_data = self.clean_subject_and_assignment()
 
         return cleaned_data
