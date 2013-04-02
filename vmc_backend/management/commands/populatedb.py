a6829d3c54c84ac710b57627642d3df47c819d79
Didii Cosmin <cosmin.didii@gmail.com>
2013-04-02 19:40:46 +0300

    added comments/submissions/usertosubjects

--- vmc_backend/management/commands/populatedb.py
+++ vmc_backend/management/commands/populatedb.py
@@ -2,6 +2,7 @@ import os
 import factory
 import datetime
 
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.core import management
 from django.contrib.auth.management.commands import changepassword 
@@ -106,7 +107,102 @@ class Command(BaseCommand):
       pca.save()
       dca.save()
       mga.save()
-              
+
+      user1 = User.objects.create_user('john', 'lennon@thebeatles.com',
+       'password')
+      user1.is_staff = False
+
+      user2 = User.objects.create_user('Cosmin', 'lennon@thebeatles.com',
+       'password')
+      user2.is_staff = False
+
+      user3 = User.objects.create_user('BogdanC', 'lennon@thebeatles.com',
+       'password')
+      user3.is_staff = False
+
+      user4 = User.objects.create_user('BogdanB', 'lennon@thebeatles.com',
+       'password')
+      user4.is_staff = False
+
+      user5 = User.objects.create_user('Andrei', 'lennon@thebeatles.com',
+       'password')
+      user5.is_staff = False
+
+      user6 = User.objects.create_user('AndreiI', 'lennon@thebeatles.com',
+       'password')
+      user6.is_staff = False
+
+      user7 = User.objects.create_user('Marius', 'lennon@thebeatles.com',
+       'password')
+      user7.is_staff = False
+
+      user8 = User.objects.create_user('Vlad', 'lennon@thebeatles.com',
+       'password')
+      user8.is_staff = False
+      
+      user1.save()            
+      user2.save()
+      user3.save()
+      user4.save()
+      user5.save()
+      user6.save()
+      user7.save()
+      user8.save()
+
+      right1 = UsersToSubjects( subject = so, user = user1, role = 'teacher')
+      right2 = UsersToSubjects( subject = sd, user = user2, role = 'student')
+      right3 = UsersToSubjects( subject = pa, user = user3, role = 'assistant')
+      right4 = UsersToSubjects( subject = uso, user = user4, role = 'assistant')
+      right5 = UsersToSubjects( subject = dc, user = user5, role = 'student')
+      right6 = UsersToSubjects( subject = mg, user = user6, role = 'student')
+      right7 = UsersToSubjects( subject = ma, user = user7, role = 'student')
+      right8 = UsersToSubjects( subject = ps, user = user8, role = 'student')
+
+      right1.save()
+      right2.save()
+      right3.save()
+      right4.save()
+      right5.save()
+      right6.save()
+      right7.save()
+      right8.save()
+
+      submission1 = Submission(student = user2,assignment = sda)
+      submission2 = Submission(student = user5,assignment = dca)
+      submission3 = Submission(student = user6,assignment = mga)
+      submission4 = Submission(student = user7,assignment = maa)
+      submission5 = Submission(student = user8,assignment = psa)
+      submission6 = Submission(student = user5,assignment = dca)
+      submission7 = Submission(student = user6,assignment = mga)
+      submission8 = Submission(student = user7,assignment = maa)
+
+      submission1.save()
+      submission2.save()
+      submission3.save()
+      submission4.save()
+      submission5.save()
+      submission6.save()
+      submission7.save()
+      submission8.save()
+
+      comment1 = SubmissionComment(submission = submission1, comment = "test1")
+      comment2 = SubmissionComment(submission = submission2, comment = "test2")
+      comment3 = SubmissionComment(submission = submission3, comment = "test3")
+      comment4 = SubmissionComment(submission = submission4, comment = "test4")
+      comment5 = SubmissionComment(submission = submission5, comment = "test5")
+      comment6 = SubmissionComment(submission = submission6, comment = "test6")
+      comment7 = SubmissionComment(submission = submission7, comment = "test7")
+      comment8 = SubmissionComment(submission = submission8, comment = "test8")
+
+      comment1.save()
+      comment2.save()
+      comment3.save()
+      comment4.save()
+      comment5.save()
+      comment6.save()
+      comment7.save()
+      comment8.save()
+
     deleteDB()
     recreateDB()
     generateData()
