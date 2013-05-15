f22ee381611ae755e434a5bbbda5921dbcb69959
Bogdan Ciobanu <bgdn.ciobanu@gmail.com>
2013-05-15 21:24:16 +0300

    Removed TODOs (#110)

--- app/conf/datasource.js
+++ app/conf/datasource.js
@@ -11,13 +11,11 @@ App.DataSourceConfig = {
                 type: 'relational',
                 collection: 'submissions',
                 url: App.general.FRONTEND_URL + '/submission/'
-                //TODO change to /submissions/ when b/#113 is merged
         },
         '/assignments': {
                 type: 'relational',
                 collection: 'assignments',
                 url: App.general.FRONTEND_URL + '/assignment/'
-                //TODO change to /assignemnts/ when b/#113 is merged
         }
     }
 };
